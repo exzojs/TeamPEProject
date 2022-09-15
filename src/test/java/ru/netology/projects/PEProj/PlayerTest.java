dd26dde11029dd6ff1a6b9e736bff2ba5ed93cf4
Павел Козодаев <paveldm744@gmail.com>
2022-09-15 17:32:09 +0300

    added test shouldPlayGame

--- src/test/java/ru/netology/projects/PEProj/PlayerTest.java
+++ src/test/java/ru/netology/projects/PEProj/PlayerTest.java
@@ -8,6 +8,19 @@ import org.junit.jupiter.api.Test;
 public class PlayerTest {
 
     @Test
+    public void shouldPlayGame() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+
+        int expected = 3;
+        int actual = player.play(game, 3);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void shouldSumGenreIfOneGame() {
         GameStore store = new GameStore();
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
